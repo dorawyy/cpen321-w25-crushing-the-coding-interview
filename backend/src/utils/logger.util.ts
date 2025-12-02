2b0d0b480e05a5eb395d56633709842d86aeb0d9
Yingying <dora93wang@gmail.com>
2025-12-02 14:14:45 -0800

    Remove eslint-disable comments from logger utility
    Removed eslint-disable comments for console methods in logger utility.

--- backend/src/utils/logger.util.ts
+++ backend/src/utils/logger.util.ts
@@ -2,19 +2,19 @@ import { sanitizeArgs, sanitizeInput } from './sanitizeInput.util';
 
 const logger = {
   info: (message: string, ...args: unknown[]) => {
-    // eslint-disable-next-line no-console
+
     process.stdout.write("[INFO] " + sanitizeInput(message) + " " + sanitizeArgs(args).join(" ") + "\n");
   },
   error: (message: string, ...args: unknown[]) => {
-    // eslint-disable-next-line no-console
+
     console.error(`[ERROR] ${sanitizeInput(message)}`, ...sanitizeArgs(args));
   },
   warn: (message: string, ...args: unknown[]) => {
-    // eslint-disable-next-line no-console
+
     console.warn(`[WARN] ${sanitizeInput(message)}`, ...sanitizeArgs(args));
   },
   debug: (message: string, ...args: unknown[]) => {
-    // eslint-disable-next-line no-console
+
     console.debug(`[DEBUG] ${sanitizeInput(message)}`, ...sanitizeArgs(args));
   },
 };
